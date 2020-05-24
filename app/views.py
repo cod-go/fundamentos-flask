438819d95242df1b1b5e4e0c2cbb2a38a7789cdd
Ricart <ricarteusuo11@gmail.com>
2020-05-24 01:11:59 -0300

    feat: aplica conceitos sobre templates

--- app/views.py
+++ app/views.py
@@ -1,6 +1,10 @@
+from flask import render_template
+
 from app import app
 
 
 @app.route('/')
 def index():
-    return 'Hello world'
+    title = "Live Flask"
+    paragraph = "Este é o primeiro teste passando valores para templates com flask"
+    return render_template('home.html', title=title, paragraph=paragraph)
