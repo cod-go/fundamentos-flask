823ea7710104f9055150aa1046f129ec4fe08d19
Ricart <ricarteusuo11@gmail.com>
2020-05-24 12:33:10 -0300

    feat: adiciona listagem de usuários

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from flask import render_template
 
 from app import app
+from app.database.connection import get_db
 
 
 @app.route('/')
@@ -8,3 +9,12 @@ def index():
     title = "Live Flask"
     paragraph = "Este é o primeiro teste passando valores para templates com flask"
     return render_template('home.html', title=title, paragraph=paragraph)
+
+
+@app.route('/users')
+def list_users():
+    cur = get_db().cursor()
+    cur.execute("SELECT * FROM users")
+    data = cur.fetchall()
+    cur.close()
+    return render_template('list.html', users=data)
