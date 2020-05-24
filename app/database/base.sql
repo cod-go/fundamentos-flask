7d187e0f9387f1b92c72cf22d1140cbcba9300a9
Ricart <ricarteusuo11@gmail.com>
2020-05-24 01:34:00 -0300

    feat: aplica conceitos sobre conexão com bd

---
+++ app/database/base.sql
@@ -0,0 +1,5 @@
+CREATE TABLE users(
+	id integer,
+	name varchar(80),
+	constraint user_pk primary key(id)
+)
---
+++ app/database/connection.py
@@ -0,0 +1,35 @@
+import sqlite3
+from flask import g
+
+from app import app
+
+DATABASE = 'app/database/database.db'
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+
+    def make_dicts(cursor, row):
+        return dict((cursor.description[idx][0], value)
+                    for idx, value in enumerate(row))
+
+    db.row_factory = make_dicts
+
+    return db
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('./database/base.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
